c881dd954bb57eae069161ba69eb09157dd852e6
Lewismosage <lewismosage@users.noreply.github.com>
2024-11-05 18:48:26 +0300

    second commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     # path for log out request
     path(route='logout', view=views.logout_request, name='logout'),
 
+    # path for get cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
